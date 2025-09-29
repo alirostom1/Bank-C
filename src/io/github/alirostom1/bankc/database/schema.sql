7ef3484aa120b39b359c50191e8f7b89fe16cd3a
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-29 17:25:29 +0100

    feat : add code generator for account number

--- src/io/github/alirostom1/bankc/database/schema.sql
+++ src/io/github/alirostom1/bankc/database/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE clients (
-    id VARCHAR(26) PRIMARY KEY,
+    id VARCHAR(36) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE accounts (
-    id VARCHAR(26) PRIMARY KEY,
+    id VARCHAR(36) PRIMARY KEY,
     number VARCHAR(50) NOT NULL UNIQUE,
     balance DOUBLE NOT NULL,
     clientId VARCHAR(26) NOT NULL,
@@ -13,18 +13,18 @@ CREATE TABLE accounts (
     FOREIGN KEY (clientId) REFERENCES clients(id)
 );
 CREATE TABLE checkings_account(
-    id VARCHAR(26) PRIMARY KEY,
+    id VARCHAR(36) PRIMARY KEY,
     overdraft_limit DOUBLE NOT NULL DEFAULT 0,
     FOREIGN KEY (id) REFERENCES accounts(id)
 );
 CREATE TABLE savings_account(
-    id VARCHAR(26) PRIMARY KEY,
+    id VARCHAR(36) PRIMARY KEY,
     interest_rate DOUBLE NOT NULL DEFAULT 0,
     FOREIGN KEY (id) REFERENCES accounts(id)
 );
 
 CREATE TABLE transactions (
-    id VARCHAR(26) PRIMARY KEY,
+    id VARCHAR(36) PRIMARY KEY,
     date TIMESTAMP NOT NULL,
     amount DOUBLE NOT NULL,
     type ENUM("TRANSFER","WITHDRAW","DEPOSIT") NOT NULL,
