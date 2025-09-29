dd8fd15b8319fbbbe9e96fd2a7d747e05c6a1a4b
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-29 10:31:52 +0100

    feat: BC-3 set up folder structure with database schema and connection and a bash script to compile and run https://alirostom2201.atlassian.net/jira/software/projects/BC/boards/134\?selectedIssue\=BC-3

---
+++ src/io/github/alirostom1/bankc/Main.java
@@ -0,0 +1,7 @@
+package io.github.alirostom1.bankc;
+
+public class Main{
+    public final static void main(String[] args){
+        System.out.println("Hello world");
+    } 
+}
---
+++ src/io/github/alirostom1/bankc/database/schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE clients (
+    id VARCHAR(26) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE accounts (
+    id VARCHAR(26) PRIMARY KEY,
+    number VARCHAR(50) NOT NULL UNIQUE,
+    balance DOUBLE NOT NULL,
+    clientId VARCHAR(26) NOT NULL,
+    accountType ENUM('SAVINGS', 'CHECKING') NOT NULL DEFAULT 'CHECKING',
+    FOREIGN KEY (clientId) REFERENCES clients(id)
+);
+CREATE TABLE checkings_account(
+    id VARCHAR(26) PRIMARY KEY,
+    overdraft_limit DOUBLE NOT NULL DEFAULT 0,
+    FOREIGN KEY (id) REFERENCES accounts(id)
+);
+CREATE TABLE savings_account(
+    id VARCHAR(26) PRIMARY KEY,
+    interest_rate DOUBLE NOT NULL DEFAULT 0,
+    FOREIGN KEY (id) REFERENCES accounts(id)
+);
+
+CREATE TABLE Transaction (
+    id VARCHAR(26) PRIMARY KEY,
+    date TIMESTAMP NOT NULL,
+    amount DOUBLE NOT NULL,
+    type ENUM("TRANSFER","WITHDRAW","DEPOSIT") NOT NULL,
+    location VARCHAR(100),
+    accountId VARCHAR(26) NOT NULL,
+    FOREIGN KEY (accountId) REFERENCES accounts(id)
+);
