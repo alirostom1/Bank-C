7ff9b886a9e903bfacd118ba0cfe14e26e51580c
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-30 14:53:50 +0100

    remove unused import

--- src/io/github/alirostom1/bankc/service/ReportServiceImpl.java
+++ src/io/github/alirostom1/bankc/service/ReportServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import io.github.alirostom1.bankc.model.entity.Account;
